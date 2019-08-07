207fff993aa3fc6b21c7da3934c026aa0b21aa77
MonoMoon <archiroz1308@gmail.com>
2019-08-07 13:41:17 +0900

    mixins

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos= [
+    {
+        id:123123,
+        title: 'Video 001',
+        description: 'funny movie',
+        views: 23,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:2223,
+            name: "monomoon",
+            email:"MM@gmail.com"
+        }
+    },
+    {
+        id:133123,
+        title: 'Video 002',
+        description: 'just movie',
+        views: 123,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:21123,
+            name: "moon",
+            email:"M1M@gmail.com"
+        }
+    },
+    {
+        id:1232,
+        title: 'Video 003',
+        description: 'sf movie',
+        views: 831,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:2223,
+            name: "monomoon",
+            email:"MM@gmail.com"
+        }
+    }
+]
