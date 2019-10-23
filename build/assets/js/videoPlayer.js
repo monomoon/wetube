6e22e96a3b4565da9191d956d03d26beeab56012
MonoMoon <archiroz1308@gmail.com>
2019-10-23 19:15:08 +0900

    heroku

--- build/assets/js/videoPlayer.js
+++ build/assets/js/videoPlayer.js
@@ -147,7 +147,7 @@ function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScreenBtn.addEventListener("click", goFullScreen);
-  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime());
   videoPlayer.addEventListener("ended", handledEnded);
   volumeRange.addEventListener("input", handleDrag);
 }
